ba6a4357a9c82f8c1dfe4b3c942b3d72cc1f501c
thearlman <49441896+thearlman@users.noreply.github.com>
2019-11-23 23:35:09 -0500

    TG2024: Added more videos etc

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ let timelineIndex = 0;
 
 //~~~~~sound variables~~~~~~//
 let hoverSound = new Audio();
-let video1 = 'videos/video1.webm';
-
+let marioSound = new Audio();
 //~~~~~load sounds into the variables  (for HTML)~~~~~~//
 hoverSound.src = 'sounds/tick.mp3';
+marioSound.src = 'sounds/mario.mp3';
 
 //~~~~~Background Video Variables~~~~~~//
 let backgroundVideoArray = ['videos/LiftOff.mp4',
@@ -23,7 +23,7 @@ let backgroundVideoIndex = 0;
 
 //~~~~~Hologram Video Variables~~~~~~//
 let hologramVideoArray = ['videos/Trump_Speech.webm',
-                        'videos/Border_Text.webm'];
+                        'videos/starman.webm'];
 let hologramVideoIndex = 0;
 
 
@@ -36,16 +36,26 @@ let cockpitColors = {"blue": 'images/cockpits/Cockpit_Blue.png',
                     "white": 'images/cockpits/Cockpit_White.png'};
 
 
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~~~~~~~PART 1: LEAVING EARTH~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//
+//
 function leavingEarth(){
+  // window.mediaDevices.getUserMedia({
+  //   audio: true,
+  //   video: true
+  // })
   timelineIndex++
   console.log(timelineArray[timelineIndex]);
   //hide the splash screen
   let splashScreen = document.getElementById('splash').style.visibility = "hidden";
   //Set the hologram button(s) visibillity
-  let buttonState = document.getElementById('button1').style.visibility = "hidden";
-  buttonState = document.getElementById('button2').style.visibility = "hidden";
-  buttonState = document.getElementById('button3').style.visibility = "hidden";
-  buttonState = document.getElementById('button4').style.visibility = "hidden";
+  let buttonState = document.getElementById('button1').style.visibility = "visible";
+  buttonState = document.getElementById('button2').style.visibility = "visible";
+  buttonState = document.getElementById('button3').style.visibility = "visible";
+  buttonState = document.getElementById('button4').style.visibility = "visible";
   //set and play the appropriate background video
   let bgVid = document.getElementById('backgroundVideo');
   bgVid.src = backgroundVideoArray[0];
@@ -78,13 +88,17 @@ function leavingEarth(){
 }
 
 
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~~~~~~~Transition 1: STAR MAN~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//
+//
 function trans1(){
   timelineIndex++
   console.log(timelineArray[timelineIndex]);
   //Set the hologram button(s) visibillity
   let buttonState = document.getElementById('button1').style.visibility = "visible";
-  buttonState = document.getElementById('button2').style.visibility = "visible";
+  buttonState = document.getElementById('button2').style.visibility = "hidden";
   buttonState = document.getElementById('button3').style.visibility = "hidden";
   buttonState = document.getElementById('button4').style.visibility = "hidden";
   //set functionality of buttons
@@ -101,7 +115,7 @@ function trans1(){
       holoVid.play();;
     };
     //set the cockpit hologram orb colors
-    let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["green"];
+    let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["purple"];
 
   //set and play the appropriate background video
   let bgVid = document.getElementById('backgroundVideo');
@@ -123,10 +137,65 @@ function trans1(){
   //check when background video is done playing
   let checkBgEnd = document.getElementById("backgroundVideo");
   checkBgEnd.onended = function() {
-    leavingEarth();
+    border();
     };
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~~PART 2: BORDERLINE CRIMINAL~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//
+//
+function border(){
+  timelineIndex++
+  console.log(timelineArray[timelineIndex]);
+  //Set the hologram button(s) visibillity
+
+  let buttonState = document.getElementById('button1').style.visibility = "visible";
+  buttonState = document.getElementById('button2').style.visibility = "visible";
+  buttonState = document.getElementById('button3').style.visibility = "hidden";
+  buttonState = document.getElementById('button4').style.visibility = "hidden";
+  //set functionality of buttons
+    //button1
+  let button1Fun = document.getElementById('button1').onclick = function(){
+    let holoVid = document.getElementById('hologramContent');
+    holoVid.src = hologramVideoArray[1];
+    holoVid.play();;
+  };
+    //button2
+    let button2Fun = document.getElementById('button2').onclick = amazonJourney();
+    //set the cockpit hologram orb colors
+    let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["red"];
+  //set and play the appropriate background video
+  let bgVid = document.getElementById('backgroundVideo');
+  bgVid.src = backgroundVideoArray[1];
+  bgVid.play();
+  //set the cockpit hologram orb colors
+  cockColor = document.getElementById('cockpitStyle');
+  cockColor.src = cockpitColors["red"];
+  //play the appropriate video on the holo-console
+  let holoVid = document.getElementById('hologramContent');
+  holoVid.src = hologramVideoArray[1];
+  holoVid.loop = true;
+  holoVid.play();
+    //check when hologram video is done playing
+    let checkHoliEnd = document.getElementById("hologramContent");
+    checkHoliEnd.onended = function() {
+    //set the cockpit hologram orb color back to white
+    let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["white"];
+    };
+  //check when background video is done playing
+  let checkBgEnd = document.getElementById("backgroundVideo");
+  checkBgEnd.onended = function() {
+    //border();
+    };
+}
+
+//opens a new window with the planet amazon
+//
+function amazonJourney(){
+  window.open("https://thearlman.github.io/Asa_Perlman-Cart_253_Fall_2019/Projects/project2", "_blank", "replace=true, scrollbars=no, top=100, left=100, width=900, height=500");
+}
 
 
 
