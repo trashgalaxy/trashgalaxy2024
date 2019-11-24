6fde2264a4d0d964ae211f4dd2cdd47844deb6b3
thearlman <49441896+thearlman@users.noreply.github.com>
2019-11-23 20:54:27 -0500

    TG2024: did some shit

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 //I hope it doens't get too cluttered
 
 //~~~~Current stage in the game~~~~
-let timelineState;
+let timelineArray = ["splash", "leavingEarth", "trans1", "border", "trans2", "museum", "trans3", "mars"];
 
+let timelineIndex = 0;
 
 //~~~~~sound variables~~~~~~//
 let hoverSound = new Audio();
@@ -13,101 +14,126 @@ let video1 = 'videos/video1.webm';
 hoverSound.src = 'sounds/tick.mp3';
 
 //~~~~~Background Video Variables~~~~~~//
-let backgroundVideoArray = ['videos/motorCycle.mp4',
-                        'videos/text_gen.webm',
-                        'videos/Road_Zoom.mp4'];
+let backgroundVideoArray = ['videos/LiftOff.mp4',
+                          'videos/space.mp4',
+                          'videos/space.mp4'];
 let backgroundVideoIndex = 0;
 
-//test
+
 
 //~~~~~Hologram Video Variables~~~~~~//
-let hologramVideoArray = ['videos/Border_Text.webm',
-                          'videos/Trump_Speech.webm'];
+let hologramVideoArray = ['videos/Trump_Speech.webm',
+                        'videos/Border_Text.webm'];
 let hologramVideoIndex = 0;
 
 
-
 //~~~~~Cockpit Image Variables~~~~~~//
-let cockpitImageArray = ['images/cockpits/Cockpit_Blue.png',
-                      'images/cockpits/Cockpit_Green.png',
-                      'images/cockpits/Cockpit_Purple.png',
-                      'images/cockpits/Cockpit_Red.png',
-                      'images/cockpits/Cockpit_White.png'];
-let cockpitImageIndex = 0;
+let cockpitColors = {"blue": 'images/cockpits/Cockpit_Blue.png',
+                    "green": 'images/cockpits/Cockpit_Green.png',
+                    "purple": 'images/cockpits/Cockpit_Purple.png',
+                    "red": 'images/cockpits/Cockpit_Red.png',
+                    "green": 'images/cockpits/Cockpit_Green.png',
+                    "white": 'images/cockpits/Cockpit_White.png'};
 
 
-
-//=======================//
-//     draw()
-//=======================//
-//
-//main loop for checking game states and triggering events
-// let vidEndTest = document.getElementById('backgroundVideo').addEventListener;
-// function draw(){
-//   if(vidEndTest.ended){
-//   console.log("done");
-// }
-// }
-
-
-
-
-function ended(){
-  console.log('ebded');
-}
-
-
-// function mousePressed(){
-//   // console.log('pressed');
-//   // let buttonState = document.getElementById('button1').style.visibility = "hidden";
-//   navigator.mediaDevices.getUserMedia(audio: true);
-// }
-
-
-
-
-function startSite(){
+function leavingEarth(){
+  timelineIndex++
+  console.log(timelineArray[timelineIndex]);
+  //hide the splash screen
   let splashScreen = document.getElementById('splash').style.visibility = "hidden";
+  //Set the hologram button(s) visibillity
+  let buttonState = document.getElementById('button1').style.visibility = "hidden";
+  buttonState = document.getElementById('button2').style.visibility = "hidden";
+  buttonState = document.getElementById('button3').style.visibility = "hidden";
+  buttonState = document.getElementById('button4').style.visibility = "hidden";
+  //set and play the appropriate background video
   let bgVid = document.getElementById('backgroundVideo');
-  let holoVid = document.getElementById('hologramContent');
+  bgVid.src = backgroundVideoArray[0];
   bgVid.play();
+  //set the cockpit hologram orb colors
+  let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["blue"];
+  //play the appropriate video on the holo-console
+  let holoVid = document.getElementById('hologramContent');
+  holoVid.src = hologramVideoArray[0];
   holoVid.play();
+  //check when hologram video is done playing
+  let checkEnd = document.getElementById("hologramContent");
+  checkEnd.onended = function() {
+  //set the cockpit hologram orb color back to white
+  let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["white"];
+  };
+  //check when hologram video is done playing
+  let checkHoliEnd = document.getElementById("hologramContent");
+  checkHoliEnd.onended = function() {
+  //set the cockpit hologram orb color back to white
+  cockColor = document.getElementById('cockpitStyle');
+  cockColor.src = cockpitColors["white"];
+  };
+  //check when background video is done playing
+  let checkBgEnd = document.getElementById("backgroundVideo");
+  checkBgEnd.onended = function() {
+    console.log();
+    trans1();
+    };
 }
 
 
-//=======================//
-//      changeBgVideo()
-//=======================//
-//
-//Changes the background video on call, by cycling through the background
-// image array
-function changeBgVideo(backgroundVideo){
-  console.log(button);
-   let bgVid = document.getElementById('backgroundVideo').src = backgroundVideoArray[backgroundVideo];
+
+function trans1(){
+  timelineIndex++
+  console.log(timelineArray[timelineIndex]);
+  //Set the hologram button(s) visibillity
+  let buttonState = document.getElementById('button1').style.visibility = "visible";
+  buttonState = document.getElementById('button2').style.visibility = "visible";
+  buttonState = document.getElementById('button3').style.visibility = "hidden";
+  buttonState = document.getElementById('button4').style.visibility = "hidden";
+  //set functionality of buttons
+    //button1
+  let button1Fun = document.getElementById('button1').onclick = function(){
+    let holoVid = document.getElementById('hologramContent');
+    holoVid.src = hologramVideoArray[1];
+    holoVid.play();;
+  };
+    //button2
+    let button2Fun = document.getElementById('button2').onclick = function(){
+      let holoVid = document.getElementById('hologramContent');
+      holoVid.src = hologramVideoArray[1];
+      holoVid.play();;
+    };
+    //set the cockpit hologram orb colors
+    let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["green"];
+
+  //set and play the appropriate background video
+  let bgVid = document.getElementById('backgroundVideo');
+  bgVid.src = backgroundVideoArray[1];
+  bgVid.play();
+  //set the cockpit hologram orb colors
+  cockColor = document.getElementById('cockpitStyle');
+  cockColor.src = cockpitColors["green"];
+  //play the appropriate video on the holo-console
+  let holoVid = document.getElementById('hologramContent');
+  holoVid.src = hologramVideoArray[1];
+  holoVid.play();
+    //check when hologram video is done playing
+    let checkHoliEnd = document.getElementById("hologramContent");
+    checkHoliEnd.onended = function() {
+    //set the cockpit hologram orb color back to white
+    let cockColor = document.getElementById('cockpitStyle').src = cockpitColors["white"];
+    };
+  //check when background video is done playing
+  let checkBgEnd = document.getElementById("backgroundVideo");
+  checkBgEnd.onended = function() {
+    leavingEarth();
+    };
 }
 
-//=======================//
-//      changeHologramVideo()
-//=======================//
-//
-//Changes the background video on call, by cycling through the background
-// image array
-function changeHologramVideo(hologramVideo){
-  console.log(button);
-   let holoVid = document.getElementById('hologramContent').src = hologramVideoArray[hologramVideo];
-}
 
 
-//=======================//
-//    changeCockpit()
-//=======================//
-//
-//Changes the background image on call, by cycling through the cockpit
-// image array
-function changeCockpit(cockpitImage){
-  console.log(button);
-   let cockpit = document.getElementById('cockpitStyle').src = cockpitImageArray[cockpitImage];
-}
+
+
+
+
+
 
 //=======================//
 //    need to fix This
